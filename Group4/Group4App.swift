f1c49b97851f781c70172cb6fc26d925c330b267
Bethan Thurgood <bethan@thurgood.org.uk>
2024-08-13 10:04:06 +0100

    Initial Commit

---
+++ Group4/Group4App.swift
@@ -0,0 +1,17 @@
+//
+//  Group4App.swift
+//  Group4
+//
+//  Created by Scholar on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Group4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
