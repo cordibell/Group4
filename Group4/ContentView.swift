a35024fed656aa4f60f25d0c7098a2da5dbeb548
leaok3 <109521386+leaok3@users.noreply.github.com>
2024-08-14 13:40:44 +0100

    Update ContentView.swift

--- Group4/ContentView.swift
+++ Group4/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Group4
+//  HomePage
 //
 //  Created by Scholar on 13/08/2024.
 //
@@ -9,16 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        } // vstack
-        .padding()
-    } // body
-} // struct
+        NavigationStack{
+            ZStack{
+                Image("background2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                
+                Image("background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .position(x: 200, y: 300)
+                
+                VStack {
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/)  {
+                        Image("start")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .position(x: 85, y: 420)
+                            .containerRelativeFrame(.horizontal) { size, axis in
+                                size * 0.4}
+                    }
+                            .toolbar{
+                                ToolbarItemGroup(placement: .status){
+                                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                        Image("quiz")
+                                    }
+                                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                        Image("resources")
+                                
+                                    }
+                                }
+                            }
 
+                                    
+                                }
+                            }
+                    }
+
+                }
+            }
 #Preview {
     ContentView()
 }
