79ca7af094b5f0102cf073af0971a8e97897bd86
Mei <meixvuong@gmail.com>
2024-08-14 15:51:57 +0100

    added questyion 1page

---
+++ Question1.swift
@@ -0,0 +1,18 @@
+//
+//  Question1.swift
+//  Group4
+//
+//  Created by Scholar on 14/08/2024.
+//
+
+import SwiftUI
+
+struct Question1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Question1()
+}
